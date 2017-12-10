4da8103e3ae9ae1cdbb30f3b536353d82bc3a448
khauck2 <karibunga@gmail.com>
2017-12-10 15:37:31 -0800

    JB feedback Sunday

--- samples/police-google-sheets/table.js
+++ samples/police-google-sheets/table.js
@@ -6,32 +6,41 @@ sheets.getData(data => {
     //loop through records and output to the screen:
     console.log(data)
     let num = 1
+    //data.records.shift()
     data.records.forEach(record => {
         const tr = document.createElement('tr')
         let td = document.createElement('td')
         td.innerHTML = num
         tr.appendChild(td)
 
+
+
         td = document.createElement('td')
-        td.innerHTML = record.pain
+        td.innerHTML = record.intensity
         tr.appendChild(td)
 
+
+/*
         td = document.createElement('td')
-        td.innerHTML = record.jan1
-        tr.appendChild(td)
-
-
-
-        td = document.createElement('td')
-        td.innerHTML = record.jan_2
+        td.innerHTML = record.feb
         tr.appendChild(td)
 
         td = document.createElement('td')
         td.innerHTML = record.mar
         tr.appendChild(td)
 
+        td = document.createElement('td')
+        td.innerHTML = record.apr
+        tr.appendChild(td)
 
+        td = document.createElement('td')
+        td.innerHTML = record.may
+        tr.appendChild(td)
 
+        td = document.createElement('td')
+        td.innerHTML = record.jun
+        tr.appendChild(td)
+*/
         //
         document.querySelector('#content tbody').appendChild(tr)
         ++num
